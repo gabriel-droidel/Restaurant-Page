b07ac4f64af800985c455fbfa320ea7962ada899
gabi droid <193822798+gabriel-droidel@users.noreply.github.com>
2025-02-07 08:28:42 +0100

    Create asset folder for images

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -1,6 +1,14 @@
 function createMenu() {
 	const divContent = document.querySelector('#content');
 	divContent.textContent = '';
+	const menuItem = [];
+
+	function createItemMenu(name, price, image) {
+		const getName = () => name;
+		const getPrice = () => price;
+		const getImage = () => image;
+	}
 }
 
 export { createMenu };
+// Photo by Cats Coming: https://www.pexels.com/photo/pizza-dish-367915/
