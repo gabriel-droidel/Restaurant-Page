10e2ad231fc716ca3bdb001de7202cddaa356835
gabi droid <193822798+gabriel-droidel@users.noreply.github.com>
2025-02-07 08:56:09 +0100

    Add constructor for menu item

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -1,14 +1,35 @@
+// Import dish images
+import pizzaImg from '../../assets/pizza.jpg';
+
 function createMenu() {
 	const divContent = document.querySelector('#content');
 	divContent.textContent = '';
-	const menuItem = [];
-
-	function createItemMenu(name, price, image) {
-		const getName = () => name;
-		const getPrice = () => price;
-		const getImage = () => image;
+	const menu = [];
+	function createItemMenu(name, price, image, description) {
+		return { name, price, image, description };
 	}
+
+	const pizza = createItemMenu('Pizza', 8, pizzaImg, 'A very tasty pizza.');
+	menu.push(pizza);
+	function displayItem(dish) {
+		const itemContainer = document.createElement('div');
+		const name = document.createElement('h3');
+		name.textContent = dish.name;
+		const price = document.createElement('p');
+		price.textContent = dish.price;
+		const image = document.createElement('img');
+		image.src = dish.image;
+		const details = document.createElement('p');
+		details.textContent = dish.description;
+		itemContainer.appendChild(name, price, image, details);
+		divContent.appendChild(itemContainer);
+		console.log('nope');
+	}
+	menu.forEach((item) => displayItem(item));
+	console.log('seen');
 }
 
 export { createMenu };
 // Photo by Cats Coming: https://www.pexels.com/photo/pizza-dish-367915/
+// Photo by Eneida Nieves: https://www.pexels.com/photo/cooked-pasta-on-brown-wooden-bowl-803963/
+// Photo by Cristian Mihaila: https://www.pexels.com/photo/classic-italian-risotto-14537683/
